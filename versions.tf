9d460119f870363bbee538a9e4c469fd9da76e4a
Nishu.Bharti1 <Nishu.Bharti1@ibm.com>
2022-07-14 12:26:37 +0530

    upgrade version

--- versions.tf
+++ versions.tf
@@ -1,4 +1,10 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    ibm = {
+      source = "ibm-cloud/ibm"
+      version = ">= 1.10.0"
+    }
+  }
+  required_version = ">= 0.13"
 }
