dae6b2e5540d033eb35730701f4fff4927f32ba9
Nishu.Bharti1 <Nishu.Bharti1@ibm.com>
2022-07-14 12:34:40 +0530

    upgrade version

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
-provider "ibm" {
-  ibmcloud_api_key   = var.ibmcloud_api_key
-}
-terraform {
-  required_providers {
-   ibm = ">= 1.11.0"
-  }
-}
+# provider "ibm" {
+#   ibmcloud_api_key   = var.ibmcloud_api_key
+# }
+# terraform {
+#   required_providers {
+#    ibm = ">= 1.11.0"
+#   }
+# }
